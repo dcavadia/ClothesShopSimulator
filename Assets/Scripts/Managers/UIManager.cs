1e99d4ef635cf33672f02b2aa7410500c2091ddf
dcavadia <dan.acppp@gmail.com>
2023-05-25 02:17:09 -0400

    add UIManager

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : SingletonComponent<UIManager>
+{
+    public GameObject UICanvas;
+    public ShopPanel ShopPanel;
+
+}
